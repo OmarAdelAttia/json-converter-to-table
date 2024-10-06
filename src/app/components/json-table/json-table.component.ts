4c0937d68dfe2bf08b56a4a13d10469ed8394cd3
Omar Adel <69749119+Omar798-c@users.noreply.github.com>
2024-10-06 20:20:02 +0300

    preparing the environment

---
+++ src/app/components/json-table/json-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-json-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './json-table.component.html',
+  styleUrl: './json-table.component.scss'
+})
+export class JsonTableComponent {
+
+}
