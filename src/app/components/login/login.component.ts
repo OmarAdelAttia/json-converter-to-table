72887e6c9a53cc7a8bccd13582e9ba6a2bd1b2d4
Omar Adel <69749119+Omar798-c@users.noreply.github.com>
2024-10-07 02:31:34 +0300

    creating reactive form logic

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,16 +2,40 @@ import { Component, signal } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [InputTextModule, PasswordModule, ButtonModule],
+  imports: [ReactiveFormsModule, InputTextModule, PasswordModule, ButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
   logoImg = signal<string>("../../../assets/imgs/image.png");
+
+  form = new FormGroup({
+    email: new FormControl(undefined, {
+      validators: [
+        Validators.email,
+        Validators.required
+      ]
+    }),
+    password: new FormControl(undefined, {
+      validators: [
+        Validators.required,
+        Validators.minLength(6)
+      ]
+    }),
+  });
+
+  onForgetPassword() {
+  }
+
+  onSubmit() {
+    const enteredEmail = this.form.value.email;
+    const enteredPassword = this.form.value.password;
+  }
 
 }
